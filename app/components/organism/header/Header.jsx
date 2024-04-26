89b19ac1076e3d9507a9e224b3ffbbf36045c6aa
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2024-04-26 18:31:28 +0200

    header

--- app/components/organism/header/Header.jsx
+++ app/components/organism/header/Header.jsx
@@ -1,29 +1,51 @@
-import MobileNav from './MobileNav';
-import Navbar from './navbar/Navbar';
+"use client"
 import Link from 'next/link';
 import Image from 'next/image';
 import './_header.scss';
 import Logo from '../../../assets/Logo.svg';
+import React, { useState, useEffect } from 'react';
 
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <header>
-      <div className="nav-area">
+      <div className="navBar">
         <Link href="/" className="logo">
           <Image 
             src={Logo}
             alt='logo'
             width={52}
             quality={100}
-            />
+          />
           Dr Hertzeg
         </Link>
+        <nav>
+          <div className="nav-links">
+            <a href="/cajevi" id="header-home">Čajevi</a>
+            <a href="/kozmetika" id="header-references">Kozmetika</a>
+            <a href="/blog" id="header-partners">Blog</a>
+            <a href="/kontakt" id="header-about">Kontakt</a>
+          </div>
+        </nav>
+      </div>
 
-        {/* for large screens */}
-        <Navbar />
-
-        {/* for small screens */}
-        <MobileNav />
+      <div id="burger" className={`burger ${isNavOpen ? 'toggle' : ''}`} onClick={toggleNav}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+      <div className={`nav-mobile ${isNavOpen ? 'is-active' : ''}`}>
+        <div className="nav-links">
+          <a href="/cajevi">Čajevi</a>
+          <a href="/kozmetika">Kozmetika</a>
+          <a href="/blog">Blog</a>
+          <a href="/kontakt">Kontakt</a>
+        </div>
       </div>
     </header>
   );
