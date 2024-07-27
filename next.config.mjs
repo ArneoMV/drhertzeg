3ec6ae3c859e18a29242e0e1f4c14157d57450d6
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2024-07-27 17:35:43 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -5,27 +5,61 @@
 // export default nextConfig;
 
 // V3
+// import withBundleAnalyzer from '@next/bundle-analyzer';
+// import path from 'path';
+
+// const nextConfig = {
+//   // distDir: "build",
+//   // vaša konfiguracija
+//   webpack: (config) => {
+//     config.resolve.extensions.push('.js', '.jsx');
+//     return config;
+//   },
+//   // Sass options
+//   sassOptions: {
+//     includePaths: [path.join(process.cwd(), 'styles')],
+//   },
+//   // Image optimization settings for export
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default withBundleAnalyzer({
+//   enabled: process.env.ANALYZE === 'true'
+// })(nextConfig);
+
 import withBundleAnalyzer from '@next/bundle-analyzer';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Resolve __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig = {
-  // distDir: "build",
-  // vaša konfiguracija
+  trailingSlash: true, // Optional: Ensures trailing slashes if required
+  distDir: 'out', // Output directory for static export
   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'url-loader'],
+    });
+
+    config.resolve.alias['~'] = path.resolve(__dirname);
+
     config.resolve.extensions.push('.js', '.jsx');
     return config;
   },
-  // Sass options
   sassOptions: {
     includePaths: [path.join(process.cwd(), 'styles')],
   },
-  // Image optimization settings for export
   images: {
     unoptimized: true,
   },
+  output: 'export', // Ensure static export
 };
 
 export default withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true'
 })(nextConfig);
-
