b7614a23ec717d19be3423c954c398613bfb8195
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2024-09-02 01:47:17 +0200

    img fix

--- app/components/organism/workInProgress/workInProgress.jsx
+++ app/components/organism/workInProgress/workInProgress.jsx
@@ -8,14 +8,14 @@ export default function WorkInProgress() {
   return (
     <div className="work-in-progress">
         <div className="card">
-            {/* <Image
-                src={Construction.animacija}
+            <Image
+                src="/images/animacija-2.gif"
                 alt='unser construction'
                 quality={100}
                 width={100}
                 height={100}
-            /> */}
-            <img src="/images/animacija-2.gif" alt="under construction" />
+            />
+            {/* <img src="/images/animacija-2.gif" alt="under construction" /> */}
             <br/><br/>
             <h5>Unser Construction</h5>
             <p>We are currently working on awsome new site.</p>
