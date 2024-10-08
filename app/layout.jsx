0a2847ae9375d4cc79015b1561a7c2cb30b94dc6
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2024-10-08 13:47:30 +0200

    new build

--- app/layout.jsx
+++ app/layout.jsx
@@ -25,26 +25,6 @@ export default function RootLayout({ children }) {
       </Head>
 
       <body>
-        {/* Google Analytics */}
-        {/* <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        /> */}
-        
         <Header />
         {children}
         <Footer />
