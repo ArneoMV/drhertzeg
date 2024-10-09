d7b7c88866de2e7cd2c4dab278665b219a31a2c7
Arneo Marin-Vincetić <arneo.marinvincetic@gmail.com>
2024-10-09 20:01:15 +0200

    GTM skripte

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,10 @@
-// import Script from 'next/script';
 import './styles/_index.scss';
 import './globals.css';
 import Header from './components/organism/header/Header.jsx';
 import Footer from './components/organism/footer/footer.jsx';
 import './styles/abstract/_typo.scss';
 import { Work_Sans } from 'next/font/google';
+import Script from 'next/script';
 
 const workSans = Work_Sans({
   subsets: ['latin'],
@@ -22,7 +22,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${workSans.variable}`}>
+
+      <head>
+        {/* Google Tag Manager Script */}
+        <Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-ML34Q7H7');`
+        }} />
+      </head>
+
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-ML34Q7H7"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
         <Header />
         {children}
         <Footer />
@@ -55,6 +68,7 @@ export default function RootLayout({ children }) {
             </filter>
           </defs>
         </svg>
+
       </body>
     </html>
   );
